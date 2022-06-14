80b077ae02eb0e6e8d7f8148ff8bd1b7897f26f9
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-14 15:28:45 -0300

    adicionando campo de pesquisa

--- contatos/urls.py
+++ contatos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('index/', views.index, name='index'),
+    path('busca/', views.busca, name='busca'),
     path('<int:contato_id>', views.ver_contato, name='ver_contato'),
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.shortcuts import redirect
+from django.db.models.functions import Concat
+from django.contrib import messages
 
 
 def inicio(request):
@@ -27,3 +31,40 @@ def ver_contato(request, contato_id):
    return render(request, 'contatos/ver_contato.html', {
        'contato': contato
    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo não pode ficar vazio.'
+        )
+        return redirect('index')
+    else:
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Pesquisa realizada.'
+        )
+
+    campos = Concat('nome', Value(' '), 'sobrenome')  # campos existente na bd. Value simula um campo da bd
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    # print(contatos.query)
+
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
+    })
