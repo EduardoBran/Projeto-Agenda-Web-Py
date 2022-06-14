9c5e9394a3dcde55c75f589e9e0ceaf360d4ac54
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-14 15:37:34 -0300

    adicionando opcao de colocar foto nos contatos

--- AGENDA/settings.py
+++ AGENDA/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- contatos/models.py
+++ contatos/models.py
@@ -18,6 +18,7 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)  # vai para tabela Categoria
     mostrar = models.BooleanField(default=True)
+    foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m')
 
     def __str__(self):
         return self.nome
