1df315aed6327a4ae7d95547d9b64026fa338b99
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-06-15 21:24:04 -0300

    requires.txt

--- AGENDA/settings.py
+++ AGENDA/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
+STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
